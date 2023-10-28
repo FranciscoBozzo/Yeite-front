fda4c8152daa56f20cca12287805c2624e158e50
Francisco Bozzo <francisco.bozzo@dosalcubo.com>
2023-10-28 03:35:41 -0300

    YEITE-1 cambia color a white

--- src/components/Song/SongEditor.jsx
+++ src/components/Song/SongEditor.jsx
@@ -18,7 +18,7 @@ function SongEditor(props) {
     return (
       <WaveSurfer
         cursorWidth="2"
-        cursorColor="#fff"
+        cursorColor="white"
         height={200}
         normalize="false"
         waveColor="white"
